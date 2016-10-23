c9ff22db977467aac7102efbc3b6dc4a3524c659
MiwaSatoya <ooo.music1157@gmail.com>
2016-10-24 01:06:47 +0900

    a mortor

--- tickle_A.ino
+++ tickle_A.ino
@@ -2,12 +2,17 @@
 #define pin2 8
 #define pinA 9
 #define MEAN 3
+#define SPEED 220
+int limit_t=895;
+int limit_b=818;
 
 int dir = 0;  // stop: 0, forward: 1, reverse: -1
 int o_val1 = 0;  // old value 1
 int o_val2 = 0;  // old value 2
-byte r_val = 0;  // resistance value
-
+int r_val = 0;  // resistance value
+static int filter[3] = {
+  0};
+int a = 0.9;
 
 void setup() {
   pinMode(pin1, OUTPUT);
@@ -18,7 +23,7 @@ void setup() {
 void loop() {
   Serial.flush();
   r_val = analogRead(A0);
-  
+
   if(Serial.available() > 0) {
     dir = Serial.read();
   }
@@ -27,22 +32,33 @@ void loop() {
     digitalWrite(pin2, LOW);
   }
   else if(dir == 1){
+    analogWrite(pinA, SPEED);
     digitalWrite(pin1, HIGH);
     digitalWrite(pin2, LOW);
   }
   else if(dir == 2){
+    analogWrite(pinA, SPEED);
     digitalWrite(pin1, LOW);
     digitalWrite(pin2, HIGH);
   }
-  
+
   int output = r_val;
-  output = NoiseCut(r_val);
+  output = noiseCut(r_val);
+  //output = rcFilter(r_val);
   Serial.println(output);
 }
 
-int NoiseCut(int in) {
+int noiseCut(int in) {
   int out = (in + o_val1 + o_val2) / MEAN;
   o_val2 = o_val1;
   o_val1 = r_val;
   return out;
 }
+
+int rcFilter(int val) {
+  filter[1] =  a * filter[0] + (1-a) * val;
+  filter[0] = filter[1];
+  return filter[0];
+}
+
+

441a931aa40a2e8fc1d613586e4f7b32e6398f39
MiwaSatoya <ooo.music1157@gmail.com>
2016-12-05 16:16:45 +0900

    Revert "1127"
    This reverts commit 2909933be5585ef61a5ed867399953e906987ec0.

--- tickle_A.ino
+++ tickle_A.ino
@@ -1,8 +1,12 @@
 const int pin[3] = {
-  4, 5, 6};  // pin number mortor
+  7, 8, 9};  // pin number mortor
+const int MEAN = 3;
+int val_hist[3];
+int val_sum = 0;
 int val_res = 0;  // resistance value mortor
 int dir = 0;  // stop: 0, forward: 1, reverse: -1
 int SPEED = 220;
+byte cnt = 0;
 
 void setup() {
   pinMode(pin[0], OUTPUT);
@@ -12,11 +16,15 @@ void setup() {
 
 void loop() {
   Serial.flush();
-  int val_a = analogRead(0);
-  if( isChanged(val_a) ) {
-    val_res = averageStream(val_a);
+  val_res = analogRead(0);
+  val_sum += val_res;
+  val_sum -= val_hist[cnt];
+  cnt++;
+  if(cnt >= MEAN) cnt = 0;
+
+  if(chkChange(channel, val)) {
+
   }
-  Serial.println(val_res);
 
   if(Serial.available() > 0) {
     dir = Serial.read();
@@ -26,19 +34,29 @@ void loop() {
     digitalWrite(pin[1], LOW);
   }
   else if(dir == 1){
-    analogWrite(pin[2], SPEED);
+    //analogWrite(pin[2], SPEED);
     digitalWrite(pin[0], HIGH);
     digitalWrite(pin[1], LOW);
   }
   else if(dir == 2){
-    analogWrite(pin[2], SPEED);
+    //analogWrite(pin[2], SPEED);
     digitalWrite(pin[0], LOW);
     digitalWrite(pin[1], HIGH);
   }
+  Serial.println(output);
+}
+
+boolean chkChange(byte channel, byte val){
+  byte chk1 = valbk[0];
+  byte chk1 = valbk[1];
+  byte chk1 = valbk[2];
+  if( (val!=chk1)&&(val!=chk2)&&(val!=chk3)&&(chk1!=chk3) ) return true;
+  if( (val!=chk1)&&(chk1==chk2)&&(chk1==chk3)&&(chk1!==0) ) return true;
+  return false;
 }
 
 int averageStream(int val){
-  static const int MODULUS = 5;
+  static const int MODULUS = 4;
   static int val_hist[MODULUS];
   static int sum  = 0;
   static byte i_mod = 0;
@@ -49,22 +67,17 @@ int averageStream(int val){
   return sum / MODULUS;
 }
 
-boolean isChanged(int val) {
-  static const int MODULUS = 5;
+boolean isChanged() {
+  static cnost int MODULUS = 4;
   static int val_hist[MODULUS];
   static byte i_mod = 0;
 
   val_hist[i_mod] = val;
   i_mod = ++i_mod % MODULUS;
   for(int i=0; i < MODULUS-1; i++) {
-    if(val == val_hist[ (i_mod+i) % MODULUS] ) {
-      return false;
-    }
+    if(val == val_hist[ (i_mod+i) % MODULUS] ) return false;
   }
   return true;
 }
-
-
-
 
 
